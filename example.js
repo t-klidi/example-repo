a2f1a91da7b9bc23aa847647bc3c7ac7cd4b4ae9
RobertKlidi <palp0gbance@gmail.com>
2023-01-14 18:45:15 +0300

    my second commot of a js file

--- example.js
+++ example.js
@@ -1,1 +1,2 @@
 const newFile = new File();
+// after learnign the basics of git and github i will embark on a the journey of making my portfolio website and add projects to that portfolio and the the website
